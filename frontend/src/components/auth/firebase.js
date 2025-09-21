09a69f514f83d057f012b769018105dba57bcb72
KartikVyas1925 <vyaskartik192@gmail.com>
2025-09-21 17:48:17 +0530

    Connecting frontend with backend(summarizing the news and showing it on UI)

--- frontend/src/components/auth/firebase.js
+++ frontend/src/components/auth/firebase.js
@@ -3,13 +3,13 @@ import { getAuth} from "firebase/auth";
 
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_AUTH_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_AUTH_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_AUTH_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_AUTH_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_AUTH_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_AUTH_MEASUREMENT_ID 
+  apiKey: import.meta.env.VITE_FIREBASE_AUTH_API_KEY || '',
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_AUTH_DOMAIN|| '',
+  projectId: import.meta.env.VITE_FIREBASE_AUTH_PROJECT_ID|| '',
+  storageBucket: import.meta.env.VITE_FIREBASE_AUTH_STORAGE_BUCKET|| '',
+  messagingSenderId: import.meta.env.VITE_FIREBASE_AUTH_MESSAGING_SENDER_ID|| '',
+  appId: import.meta.env.VITE_FIREBASE_AUTH_APP_ID|| '',
+  measurementId: import.meta.env.VITE_FIREBASE_AUTH_MEASUREMENT_ID|| '' 
 };
 
 const app = initializeApp(firebaseConfig);
