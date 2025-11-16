1d160d98dfb5a4b37693b0fec6fe1f6f9c005f9e
tirthgandhi9905 <gandhitirth604@gmail.com>
2025-11-16 17:41:15 +0530

    db : updated profile model and ddl script

--- backend/models/Profile.js
+++ backend/models/Profile.js
@@ -23,6 +23,12 @@ module.exports = (sequelize) => {
       type: DataTypes.TEXT,
       allowNull: true,
     },
+    email: {
+     type: DataTypes.TEXT,
+     allowNull: false,
+     unique: true,
+      comment: "User email stored in profiles table",
+    },
     avatar_url: {
       type: DataTypes.TEXT,
       allowNull: true,
